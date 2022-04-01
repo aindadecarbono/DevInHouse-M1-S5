55a118e54a8920fe85fb0d3f921d757a05a49bd9
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-01 10:18:48 -0300

    Exercício 4

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-class Produto {
-  nome
-  preço
-  emEstoque
-  quantidade
-  constructor(nome, preço, emEstoque, quantidade) {
-    this.nome = nome
-    this.preço = preço
-    this.emEstoque = emEstoque
-    this.quantidade = quantidade
+class Pedidos {
+  numeroPedido
+  dataPedido
+  estaPago
+  listaProdutos
+  nomeCliente
+  constructor(numeroPedido, nomeCliente) {
+    this.numeroPedido = numeroPedido
+    this.dataPedido = new Date().toLocaleDateString()
+    this.estaPago = false
+    this.listaProdutos = []
+    this.nomeCliente = nomeCliente
   }
 }
+
+console.log(new Pedidos(123, 'Nome Sobrenome'))
