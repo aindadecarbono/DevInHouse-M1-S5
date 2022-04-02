ecde5d58910961bf14dad41bcc35e44e797ffc34
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-01 21:26:51 -0300

    Exercício 5

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+class Produto {
+  nomeProduto
+  preço
+  emEstoque
+  quantidade
+  constructor(nomeProduto, preço, emEstoque, quantidade) {
+    this.nomeProduto = nomeProduto
+    this.preço = preço
+    this.emEstoque = emEstoque
+    this.quantidade = quantidade
+  }
+}
+
 class Pedidos {
   numeroPedido
   dataPedido
   estaPago
-  listaProdutos
   nomeCliente
+  listaProdutos
+  adicionarProduto(produto) {
+    if (produto instanceof Produto) {
+      this.listaProdutos.push(produto)
+    }
+  }
   constructor(numeroPedido, nomeCliente) {
     this.numeroPedido = numeroPedido
     this.dataPedido = new Date().toLocaleDateString()
@@ -13,4 +31,11 @@ class Pedidos {
   }
 }
 
-console.log(new Pedidos(123, 'Nome Sobrenome'))
+const produtoNovo1 = new Produto('telefone', 199, true, 1)
+const produtoNovo2 = new Produto('carro', 29999, true, 1)
+const pedidoNovo = new Pedidos(1, 'Bruno')
+
+pedidoNovo.adicionarProduto(produtoNovo1)
+pedidoNovo.adicionarProduto(produtoNovo2)
+
+console.log(pedidoNovo)
