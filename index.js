5862a61fe61ab36b66318612b7f838a101a766a6
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-03 18:55:56 -0300

    Exercício 10

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-function mostrarHorario() {
-  setInterval(() => {
-    console.log(new Date().toLocaleTimeString())
-  }, 2000)
+function sleep(valor) {
+  let minhaPromise = new Promise((resolve, reject) => {
+    if (valor) {
+      setTimeout(() => {
+        resolve(valor)
+      }, 3000)
+    } else {
+      reject('Erro')
+    }
+  })
+
+  minhaPromise.then(
+    valor => console.log(valor),
+    valor => console.log(valor)
+  )
 }
 
-mostrarHorario()
+sleep('valor qualquer')
